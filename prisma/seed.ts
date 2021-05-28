b3c28c4bd732d1e991cd0edaaafc5deff69095ea
Rem0ld <p.lovergne@hotmail.fr>
2021-05-28 20:52:25 +0200

    comment done

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -50,13 +50,107 @@ const afghan = {
   cioc: "AFG",
 };
 
-async function main() {
-  const afgha = await prisma.country.upsert({
-    where: { name: "Afghanistan" },
-    update: {},
-    create: {
-      ...afghan,
+const aland = {
+  name: "Åland Islands",
+  topLevelDomain: [".ax"],
+  alpha2Code: "AX",
+  alpha3Code: "ALA",
+  callingCodes: ["358"],
+  capital: "Mariehamn",
+  altSpellings: ["AX", "Aaland", "Aland", "Ahvenanmaa"],
+  region: "Europe",
+  subregion: "Northern Europe",
+  population: 28875,
+  latlng: [60.116667, 19.9],
+  demonym: "Ålandish",
+  area: 1580.0,
+  gini: null,
+  timezones: ["UTC+02:00"],
+  borders: [],
+  nativeName: "Åland",
+  numericCode: "248",
+  currencies: [{ code: "EUR", name: "Euro", symbol: "€" }],
+  languages: [
+    { iso639_1: "sv", iso639_2: "swe", name: "Swedish", nativeName: "svenska" },
+  ],
+  translations: {
+    de: "Åland",
+    es: "Alandia",
+    fr: "Åland",
+    ja: "オーランド諸島",
+    it: "Isole Aland",
+    br: "Ilhas de Aland",
+    pt: "Ilhas de Aland",
+    nl: "Ålandeilanden",
+    hr: "Ålandski otoci",
+    fa: "جزایر الند",
+  },
+  flag: "https://restcountries.eu/data/ala.svg",
+  regionalBlocs: [
+    {
+      acronym: "EU",
+      name: "European Union",
+      otherAcronyms: [],
+      otherNames: [],
     },
+  ],
+  cioc: "",
+};
+
+const albania = {
+  name: "Albania",
+  topLevelDomain: [".al"],
+  alpha2Code: "AL",
+  alpha3Code: "ALB",
+  callingCodes: ["355"],
+  capital: "Tirana",
+  altSpellings: ["AL", "Shqipëri", "Shqipëria", "Shqipnia"],
+  region: "Europe",
+  subregion: "Southern Europe",
+  population: 2886026,
+  latlng: [41.0, 20.0],
+  demonym: "Albanian",
+  area: 28748.0,
+  gini: 34.5,
+  timezones: ["UTC+01:00"],
+  borders: ["MNE", "GRC", "MKD", "KOS"],
+  nativeName: "Shqipëria",
+  numericCode: "008",
+  currencies: [{ code: "ALL", name: "Albanian lek", symbol: "L" }],
+  languages: [
+    { iso639_1: "sq", iso639_2: "sqi", name: "Albanian", nativeName: "Shqip" },
+  ],
+  translations: {
+    de: "Albanien",
+    es: "Albania",
+    fr: "Albanie",
+    ja: "アルバニア",
+    it: "Albania",
+    br: "Albânia",
+    pt: "Albânia",
+    nl: "Albanië",
+    hr: "Albanija",
+    fa: "آلبانی",
+  },
+  flag: "https://restcountries.eu/data/alb.svg",
+  regionalBlocs: [
+    {
+      acronym: "CEFTA",
+      name: "Central European Free Trade Agreement",
+      otherAcronyms: [],
+      otherNames: [],
+    },
+  ],
+  cioc: "ALB",
+};
+
+async function main() {
+  const countries = await prisma.country.createMany({
+    data: [
+      afghan,
+      aland,
+      albania
+    ]
   });
 
   const pierre = await prisma.user.upsert({
@@ -69,13 +163,30 @@ async function main() {
       city: "Paris",
       zip: "75000",
       travel: {
-        create: {
-          destination: "Afghanistan",
-          fromCountry: "France",
-          departureDate: new Date(),
-          done: true,
-        },
+        createMany: {
+          data: [
+            {
+              destination: "Afghanistan",
+              fromCountry: "France",
+              departureDate: new Date(),
+              done: false,
+            },
+            {
+              destination: "Albania",
+              fromCountry: "France",
+              departureDate: new Date(),
+              done: true,
+            }
+          ]
+        }
       },
+      review: {
+        create: {
+          comment: "C T tro bi1!",
+          score: 5,
+          countryId: 3
+        }
+      }
     },
   });
 
@@ -89,16 +200,42 @@ async function main() {
       city: "Roma",
       zip: "15463",
       travel: {
-        create: {
-          destination: "Afghanistan",
-          fromCountry: "Italy",
-          departureDate: new Date(),
-          done: false,
-        },
+        createMany: {
+          data: [
+            {
+              destination: "Åland Islands",
+              fromCountry: "Italy",
+              departureDate: new Date(),
+              done: false,
+            },
+            {
+              destination: "Albania",
+              fromCountry: "Italy",
+              departureDate: new Date(),
+              done: true,
+            }
+          ]
+        }
       },
+      review: {
+        createMany: {
+          data: [
+            {
+              comment: "Peu de beau paysage",
+              score: 2,
+              countryId: 2
+            },
+            {
+              comment: "Magnifique",
+              score: 4,
+              countryId: 1
+            }
+          ]
+        }
+      }
     },
   });
-  console.log({ pierre, pierrot, afgha });
+  console.log({ pierre, pierrot, countries });
 }
 
 main()
